aa86947003916d8de3a5ec47c4210a525e3d6160
mlm-games <125530737+mlm-games@users.noreply.github.com>
2026-02-03 06:11:59 +0530

    bump to agp 9.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,12 +1,7 @@
 @file:Suppress("UnstableApiUsage")
 
-import com.android.build.gradle.internal.api.ApkVariantOutputImpl
-import org.jetbrains.kotlin.gradle.dsl.JvmTarget
-
-
 plugins {
     alias(libs.plugins.android.application)
-    alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
     alias(libs.plugins.kotlin.ksp)
     alias(libs.plugins.kotlin.serialization)
