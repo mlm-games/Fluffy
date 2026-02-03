2185bc8598dbeb2c11dba574d4d42eabc46062e4
ragebreaker <125530737+mlm-games@users.noreply.github.com>
2026-02-03 07:21:30 +0530

    chore: Update versionName to 3.7.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -27,7 +27,7 @@ android {
         minSdk = 24
         targetSdk = 36
         versionCode = 630
-        versionName = "3.6.2"
+        versionName = "3.7.0"
 
         androidResources {
             localeFilters += setOf("en", "ar", "de", "es-rES", "es-rUS", "fr", "hr", "hu", "in", "it", "ja", "pl", "pt-rBR", "ru-rRU", "sv", "tr", "uk", "zh")
