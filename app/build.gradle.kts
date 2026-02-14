c39f175210db3b42df9d32e0e8e697e4371e5fff
ragebreaker <125530737+mlm-games@users.noreply.github.com>
2026-02-14 09:46:12 +0530

    chore: for prev. rel, but missed

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -28,7 +28,7 @@ android {
         minSdk = 24
         targetSdk = 36
         versionCode = 660
-        versionName = "4.0.3"
+        versionName = "4.1.0"
 
         androidResources {
             localeFilters += setOf("en", "ar", "de", "es-rES", "es-rUS", "fr", "hr", "hu", "in", "it", "ja", "pl", "pt-rBR", "ru-rRU", "sv", "tr", "uk", "zh")
